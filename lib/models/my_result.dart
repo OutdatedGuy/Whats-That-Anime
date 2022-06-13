3e4dce52cca91658f03a8fc2a36f9df0ce276e08
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-06-13 19:38:20 +0530

    new: created model for process results

---
+++ lib/models/my_result.dart
@@ -0,0 +1,18 @@
+enum ResultStatus {
+  success,
+  warning,
+  error,
+}
+
+class MyResult {
+  ResultStatus status;
+  String code;
+  String? message, url;
+
+  MyResult({
+    required this.status,
+    required this.code,
+    this.message,
+    this.url,
+  });
+}
