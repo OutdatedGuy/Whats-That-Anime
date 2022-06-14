e77884eb7a01c14aa089f3fbb6171e6f7ebb43b7
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-06-15 03:51:53 +0530

    feat: Implemented theme change functionality

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+// Dart Packages
+import 'dart:async';
+
 // Firebase Packages
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 // Third Party Packages
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:get_storage/get_storage.dart';
+
+// Data Models
+import 'package:whats_that_anime/models/user_preferences.dart';
 
 // Themes
 import 'themes/app_theme.dart';
@@ -33,14 +40,44 @@ Future<void> main() async {
       ),
     ],
   );
-
-  FirebaseAuth.instance.signInAnonymously();
+  await GetStorage.init(
+    FirebaseAuth.instance.currentUser?.uid ?? 'GetStorage',
+  );
 
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late StreamSubscription<User?> _userSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _userSubscription = FirebaseAuth.instance.userChanges().listen(
+      (User? user) async {
+        await GetStorage.init(user?.uid ?? 'GetStorage');
+        GetStorage(
+          user?.uid ?? 'GetStorage',
+        ).listenKey('theme', (value) => setState(() {}));
+
+        setState(() {});
+      },
+    );
+    FirebaseAuth.instance.signInAnonymously();
+  }
+
+  @override
+  void dispose() {
+    _userSubscription.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -48,6 +85,7 @@ class MyApp extends StatelessWidget {
       title: 'What\'s That Anime',
       theme: appTheme(Brightness.light),
       darkTheme: appTheme(Brightness.dark),
+      themeMode: UserPreferences().theme,
       home: const MainScreen(),
       builder: EasyLoading.init(),
     );
