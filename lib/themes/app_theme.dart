9bfed7303a43c495dbdfbfd972827fe642264613
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-06-21 23:10:22 +0530

    (Android) FEAT: making appbar extend status bar
    - Merge appBar and status bar as one color/theme

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -1,19 +1,26 @@
 // Flutter Packages
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 // Themes
 import 'swatchs.dart';
 
 /// Returns a customized [ThemeData] object for the given [Brightness].
 ThemeData appTheme(Brightness mode) {
+  bool isLight = mode == Brightness.light;
+
   return ThemeData(
     brightness: mode,
     useMaterial3: true,
-    primarySwatch:
-        mode == Brightness.light ? lightThemeSwatch : darkThemeSwatch,
+    primarySwatch: isLight ? lightThemeSwatch : darkThemeSwatch,
     appBarTheme: AppBarTheme(
-      backgroundColor: mode == Brightness.light ? lightThemeSwatch : null,
+      backgroundColor: isLight ? lightThemeSwatch : null,
       foregroundColor: Colors.white,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.light,
+        systemStatusBarContrastEnforced: false,
+      ),
     ),
   );
 }
