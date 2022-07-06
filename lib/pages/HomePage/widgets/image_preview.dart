6e911c18580a367e8d440e04ae13bb16d64768d2
OutdatedGuy <74326345+OutdatedGuy@users.noreply.github.com>
2022-07-06 19:56:25 +0530

    FEAT: Show dashed border when no image is selected
    - Using dotted_border package

--- lib/pages/HomePage/widgets/image_preview.dart
+++ lib/pages/HomePage/widgets/image_preview.dart
@@ -6,6 +6,9 @@ import 'package:image_picker/image_picker.dart';
 // Dart Packages
 import 'dart:io';
 
+// Third Party Packages
+import 'package:dotted_border/dotted_border.dart';
+
 class ImagePreview extends StatelessWidget {
   const ImagePreview({
     Key? key,
@@ -17,39 +20,40 @@ class ImagePreview extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width * 0.8,
-      constraints: const BoxConstraints(minWidth: 100, maxWidth: 600),
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: Theme.of(context).colorScheme.primary,
-          width: 1,
-        ),
+    return DottedBorder(
+      color: Theme.of(context).colorScheme.primary,
+      borderType: BorderType.RRect,
+      strokeWidth: 2,
+      strokeCap: StrokeCap.round,
+      dashPattern: [_image == null ? 5 : 1],
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.8,
+        constraints: const BoxConstraints(minWidth: 100, maxWidth: 600),
         color: _image != null ? Colors.black87 : null,
-      ),
-      child: AspectRatio(
-        aspectRatio: 16 / 9,
-        child: Center(
-          child: _image == null
-              ? Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Text('No Image Selected'),
-                    SizedBox(height: 10),
-                    Text('Drag and drop an image here'),
-                    SizedBox(height: 10),
-                    Icon(Icons.file_download_outlined),
-                  ],
-                )
-              : kIsWeb
-                  ? Image.network(
-                      _image!.path,
-                      errorBuilder: (p0, p1, p2) => const Placeholder(),
-                    )
-                  : Image.file(
-                      File(_image!.path),
-                      errorBuilder: (p0, p1, p2) => const Placeholder(),
-                    ),
+        child: AspectRatio(
+          aspectRatio: 16 / 9,
+          child: Center(
+            child: _image == null
+                ? Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text('No Image Selected'),
+                      SizedBox(height: 10),
+                      Text('Drag and drop an image here'),
+                      SizedBox(height: 10),
+                      Icon(Icons.file_download_outlined),
+                    ],
+                  )
+                : kIsWeb
+                    ? Image.network(
+                        _image!.path,
+                        errorBuilder: (p0, p1, p2) => const Placeholder(),
+                      )
+                    : Image.file(
+                        File(_image!.path),
+                        errorBuilder: (p0, p1, p2) => const Placeholder(),
+                      ),
+          ),
         ),
       ),
     );
